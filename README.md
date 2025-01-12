490ee4710c0fdd5344bd27ea6669eeb006e4c694
Joseph Gurak <151475307+JosephGurak@users.noreply.github.com>
2025-01-12 00:01:10 -0500

    [Update] Added kelvin temp conversions  and updated readme
    Updated the temperature conversions module to include Kelvin conversions, added more to readme file.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # mathsharp
 Rust Crate for Working with Mathematical Operations 
+
+The Goal of mathsharp is to provide a fast and friendly experience for
+anyone needing to use math in their projects, from unit converisons to 
+calculus and beyond. 
+
+## Example
+
+Below example demonstrates using a temperature conversion function.
+
+```
+use mathsharp::fahrenheit_celsius;
+
+fn main() {
+    let from_fahrenheit = 32.0;
+    let to_celsius = fahrenheit_celsius(from_fahrenheit);
+    
+    println!("{} fahrenheit is equal to {} Celsius", from_fahrenheit, to_celsius);   
+}
+```
+
+## Roadmap
+
+### Official Roadmap Coming soon! 
+
+MathSharp will be starting with unit conversions and work on adding areas of 
+mathematics in consistent updates. Roadmap will be released February 1st 2025
+alongside its next minor update. 
+
+
+
+
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,14 @@
 
 pub use self::temperature::celsius_fahrenheit;
 pub use self::temperature::fahrenheit_celsius;
+pub use self::temperature::celsius_kelvin;
+pub use self::temperature::fahrenheit_kelvin;
+pub use self::temperature::kelvin_fahrenheit;
+pub use self::temperature::kelvin_celsius;
 
 
 pub mod temperature {
-    /// Converts Temperatures.
+    /// Converts Fahrenheit to Celsius.
     /// 
     /// #Examples
     /// 
@@ -25,12 +29,11 @@ pub mod temperature {
         //println!("Celsius: {:?}", temp_final);
 
         temp_final
-
     }
 
 
 
-    /// Converts Temperatures.
+    /// Converts Celsius to Fahrenheit.
     /// 
     /// #Examples
     /// 
@@ -45,8 +48,80 @@ pub mod temperature {
         let temp_final = (celsius * 1.8) + 32.0;
 
         temp_final
-
         
     }
+
+    /// Converts Celsius to Kelvin.
+    /// 
+    /// #Examples
+    /// 
+    /// ```
+    /// let celsius = 0.0;
+    /// let answer = mathsharp::celsius_kelvin(celsius);
+    /// 
+    /// assert_eq!(273.15, answer);
+    /// ```
+
+    pub fn celsius_kelvin(celsius: f32) -> f32 {
+        let temp_final = celsius + 273.15;
+
+        temp_final
+        
+    }
+
+    /// Converts Fahrenheit to Kelvin.
+    /// 
+    /// #Examples
+    /// 
+    /// ```
+    /// let fahrenheit = 32.0;
+    /// let answer = mathsharp::fahrenheit_kelvin(fahrenheit);
+    /// 
+    /// assert_eq!(273.15, answer);
+    /// ```
+
+    pub fn fahrenheit_kelvin(fahrenheit: f32) -> f32 {
+        let temp_final = (fahrenheit - 32.0)/1.8 + 273.15;
+
+        temp_final
+        
+    }
+    
+    /// Converts Kelvin to Fahrenheit.
+    /// 
+    /// #Examples
+    /// 
+    /// ```
+    /// let kelvin = 310.15;
+    /// let answer = mathsharp::kelvin_fahrenheit(kelvin);
+    /// 
+    /// assert_eq!(98.6, answer);
+    /// ```
+
+    pub fn kelvin_fahrenheit(kelvin: f32) -> f32 {
+        let temp_final = ((kelvin - 273.15)*9.0)/5.0 + 32.0;
+
+        temp_final
+        
+    }
+
+    /// Converts Kelvin to celsius.
+    /// 
+    /// #Examples
+    /// 
+    /// ```
+    /// let kelvin = 273.15;
+    /// let answer = mathsharp::kelvin_celsius(kelvin);
+    /// 
+    /// assert_eq!(0.0, answer);
+    /// ```
+
+    pub fn kelvin_celsius(kelvin: f32) -> f32 {
+        let temp_final = kelvin - 273.15;
+
+        temp_final
+        
+    }
+
 }
 
